9b6d333291834cf8c3437cdfb1275e3d91f4dc43
Nazarii <nazrii.sliusar@gmail.com>
2025-05-03 15:40:11 +0300

    Commit_4
    Виправлено  Game_board, додано метод для скидання використаних клітинок. Domino_solver додано скидування розвязку для коректної роботи solve, додано метод розвязку доміно підбором значень

--- task14.cc
+++ task14.cc
@@ -6,6 +6,7 @@
 #define R 10
 #define NIMIOUS (L*R-56)
 #define MAX_DOMINOES 28
+#define TOTAL_ELEMENTS (L*R)
 
 using namespace std;
 
@@ -44,39 +45,53 @@ class Game_board{
             for (int j = 0; j < R; ++j) {
                 board[i][j]=domino_matrix_draft[i][j];
                 used[i][j]=0;
+            }
         }
     }
-}
 
-bool isValid(int c, int u ){
-    return c>=0 && u>=0 && c<L && u<R;
-}
-
-bool isUsed(int c, int u ){
-    return used[c][u];
-}
-
-int getValue(int c, int u ){
-    return board[c][u];
-}
-
-void setValue(int c, int u, int value){
-    board[c][u] =value;
-}
-
-void setUsed(int c, int u, bool value=true){
-    used[L][R]=value;
-}
-
+    bool isValid(int c, int u ){
+        return c>=0 && u>=0 && c<L && u<R;
+    }
+    
+    bool isUsed(int c, int u ){
+        return used[c][u];
+    }
+    
+    int getValue(int c, int u ){
+        return board[c][u];
+    }
+    
+    void setValue(int c, int u, int value){
+        board[c][u] =value;
+    }
+    
+    void setUsed(int c, int u, bool value=true){
+        used[c][u]=value;
+    }
+    
+    void resetUsed(){
+        for(int i=0; i<L;++i){
+            for(int j=0; j<R; ++j){
+                used[i][j]=false;
+            }
+        }
+    }
 };
 
 class Domino_solver{
+    public:
     const int dx[2]={0,1};
     const int dy[2]={1,0};
     set<pair<int,int>> used_Dominoes;
     Game_board work_board;
     Domino solution_arr[MAX_DOMINOES];
     int solution_size = 0;
+    
+    void reset_solver(){
+        solution_size=0;
+        used_Dominoes.clear();
+        work_board.resetUsed();
+    }
 
     bool solve(){
         for (int i = 0; i < L; i++){
@@ -108,12 +123,12 @@ class Domino_solver{
             return true;
         }
 
-    void find_repeat(int domino_matrix[L][R], int more_8[NIMIOUS], int &size){
+    void find_repeat(const int domino_matrix1[L][R], int more_8[NIMIOUS], int &size){
         int counts[7] = {0};
         size = 0;
         for (int i = 0; i < L; i++){
             for (int j = 0; j < R; j++){
-                int val = domino_matrix[i][j];
+                int val = domino_matrix1[i][j];
                 if (val >= 0 && val <= 6){
                     counts[val]++;
                 }
@@ -125,6 +140,72 @@ class Domino_solver{
                 more_8[size++] = i;
             }
         }
+    }
+
+    bool solve_selection(const int domino_matrix2[L][R], const int target_digits[NIMIOUS], int target_digits_size){
+        int reminder_counts[7]={0};
+        for(int i=0; i<NIMIOUS; ++i){
+            int digit = target_digits[i];
+            reminder_counts[digit]++;
+        }
+        int indices[NIMIOUS];
+
+        for(int index_0 = 0; index_0 <= TOTAL_ELEMENTS - NIMIOUS; ++index_0){
+            int val_0 = domino_matrix2[index_0/R][index_0%R];
+            if(reminder_counts[val_0]>0){
+                indices[0]=index_0;
+                int reminder_counts_draft1[7];
+                copy(reminder_counts, reminder_counts+7, reminder_counts_draft1);
+                reminder_counts_draft1[val_0]--;
+
+                for(int index_1 = index_0 + 1; index_1 <= TOTAL_ELEMENTS - (NIMIOUS-1); ++index_1){
+                    int val_1 = domino_matrix2[index_1/R][index_1%R];
+                    if(reminder_counts_draft1[val_1]>0){
+                        indices[1]=index_1;
+                        int reminder_counts_draft2[7];
+                        copy(reminder_counts_draft1, reminder_counts_draft1+7, reminder_counts_draft2);
+                        reminder_counts_draft2[val_1]--;
+
+                        for(int index_2 = index_1 + 1; index_2 <= TOTAL_ELEMENTS - (NIMIOUS-2); ++index_2){
+                            int val_2 = domino_matrix2[index_2/R][index_2%R];
+                            if(reminder_counts_draft2[val_2]>0){
+                                indices[2]=index_2;
+                                int reminder_counts_draft3[7];
+                                copy(reminder_counts_draft2, reminder_counts_draft2+7, reminder_counts_draft3);
+                                reminder_counts_draft3[val_2]--;
+
+                                for(int index_3 = index_2 + 1; index_3 < TOTAL_ELEMENTS; ++index_3){
+                                    int val_3 = domino_matrix2[index_3/R][index_3%R];
+                                   
+                                    if(reminder_counts_draft3[val_3]>0){
+                                        indices[3]=index_3;
+                                        for(int row=0; row<L; ++row) {
+                                            for(int col=0; col<R; ++col) {
+                                                this->work_board.setValue(row, col, domino_matrix2[row][col]);
+                                            }
+                                        }
+                                        for(int idx=0;idx<NIMIOUS; ++idx){
+                                            int r = indices[idx]/R;
+                                            int c = indices[idx]%R;
+                                            this->work_board.setValue(r,c,-1);
+                                        }
+                                        if(this->solve()){
+                                            cout<<"Розвязок головоломки знайдено \n";
+                                            for(int solve_idx=0; solve_idx < this->solution_size; ++solve_idx){
+                                                this->solution_arr[solve_idx].print();
+                                            }
+                                            return true;
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                        }            
+                    }
+                }        
+            }
+        }
+        return false;
     }
 };
    
